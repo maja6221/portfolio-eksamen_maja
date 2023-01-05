b8e93d4eb423f3c5a11753e9ccb1b8a2413bd8cb
Maja Goltermann <maja.goltermann@gmail.com>
2023-01-05 12:57:32 +0100

    ny

---
+++ java.js
@@ -0,0 +1,8 @@
+//-------------------------------------------------------tilbage til temaer-------------------------------------------------------//
+const btn1 = document.querySelector("#tilbagepil");
+
+function tilbage() {
+  window.location.href = "temaer.html";
+}
+
+btn1.addEventListener("click", tilbage);
